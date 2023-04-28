96bd3f3b98bdb2c5da3a252d97ae34fa743263f4
manugordon <manuelgordon25@gmail.com>
2023-04-28 13:45:07 -0300

    Request Row Content

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -9,16 +9,42 @@ class RequestNew extends Component {
     state = {
         value:'',
         description:'',
-        recipient:''
+        recipient:'',
+        errorMessage:'',
+        loading: false
     }
     static async getInitialProps(props){
         const {address}= props.query;
         return{address}
     }
+
+    onSubmit = async event =>{
+        event.preventDefault();
+        const campaign = Campaign(this.props.address);
+        const {description,value,recipient} = this.state;
+        this.setState({loading:true,errorMessage:''})
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.createRequest(
+                description,
+                web3.utils.toWei(value,'ether'),
+                recipient
+            ).send({from:accounts[0]});
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`)
+        } catch (error) {
+            this.setState({errorMessage:error.message});
+        }
+
+        this.setState({loading:false})
+    }
     render(){
         return (
             <Layout>
-            <Form>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                <a>Back</a>
+                </Link>
+                <h3>Create a request</h3>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Description</label>
                     <Input 
@@ -38,7 +64,8 @@ class RequestNew extends Component {
                     value={this.state.recipient}
                     onChange={event => {this.setState({recipient:event.target.value})}}/>
                 </Form.Field>
-                <Button primary>Create</Button>
+                <Message error header='Oops' content={this.state.errorMessage}></Message>
+                <Button loading={this.state.loading} primary>Create</Button>
             </Form>
             </Layout>
         )
